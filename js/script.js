1f8ae5f295129aead4c64b78114d845988d0e31c
Alexei_Y <alexei.yarosh.web@gmail.com>
2020-10-09 14:18:01 +0300

    added swiper js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,30 @@
-(function () {
-  const burger = document.querySelector('.burger');
-  burger.addEventListener('click', () => {
-      burger.classList.toggle('burger__active');
-  });
-}());
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  (function () {
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger__active');
+    });
+  }());
+  
+  const slider = document.querySelector('.swiper-container');
+  
+  let mySwiper = new Swiper(slider, {
+    loop: true,
+    spaceBetween: 10,
+    speed: 550, 
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  
+  })
+
+});
 
 
 
-
-
-
-
-// document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
-//   item.addEventListener('click', () => {
-
-//     const parent = item.parentNode;
-
-//     parent.classList.toggle('accordion-item__active');
-//   })
-// )
-
-// .accordion-item {
-//   margin-top: 20px;
-// }
-
-// .accordion-item__trigger {
-//   padding: 20px;
-//   border: 1px solid #000;
-//   user-select: none;
-// }
-
-// .accordion-item__content {
-//   display: none;
-//   padding: 20px;
-//   border: 1px solid #000;
-//   border-top: none;
-// }
-
-// .accordion-item__active .accordion-item__trigger {
-//   background-color: #ccc;
-// }
-
-// .accordion-item__active .accordion-item__content {
-//   display: block;
-// }
