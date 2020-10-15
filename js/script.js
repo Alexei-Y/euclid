d45e484f4a709192f2bdfae00638ca134b35f241
Alexei_Y <alexei.yarosh.web@gmail.com>
2020-10-15 18:35:03 +0300

    add mob-menu

--- js/script.js
+++ js/script.js
@@ -4,9 +4,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
   (function () {
     const burger = document.querySelector('.burger');
-    burger.addEventListener('click', () => {
+    const menu = document.querySelector('.navbar');
+    const links = document.querySelector('.navbar__item');
+
+    burger.addEventListener('click', (e) => {
+      e.preventDefault();
+
         burger.classList.toggle('burger__active');
-    });
+        menu.classList.toggle('active');
+        document.querySelector('body').classList.toggle('no-scroll');
+
+      // if(burger.classList.contains('burger__active')) {
+      //   burger.classList.remove('burger__active');
+      //   menu.classList.remove('active');
+      //   document.querySelector('body').classList.remove('no-scroll');
+      // } else {
+      //   burger.classList.add('burger__active');
+      //   menu.classList.add('active');
+      //   document.querySelector('body').classList.add('no-scroll');
+      // }
+
+      links.addEventListener('click', () => {
+        burger.classList.remove('burger__active');
+        menu.classList.remove('active');
+        document.querySelector('body').classList.remove('no-scroll');
+      });
+      
+    })
+
+    // https://issue.life/questions/54786905    (fix ios) !!!!
   }());
   
   const slider = document.querySelector('.swiper-container');
